2578d87fe0630e1ae479e6b15326c26df08573a6
erichuang0771 <visualsolver@gmail.com>
2017-02-13 08:58:30 +0800

    add Training part & testing now

--- ResNet_Train.py
+++ ResNet_Train.py
@@ -4,8 +4,8 @@ import os
 with open('res50_train_val_dy.prototxt') as fin:
 	with open( 'resnet_pose.prototxt', 'wb') as fout:
 		for line in fin:
-			line = line.replace('{TRAIN_PATH}', '../DataMaker/hdf5_train_data_list.txt')
-			line = line.replace('{TEST_PATH}', '../DataMaker/hdf5_test_data_list.txt')
+			line = line.replace('{TRAIN_PATH}', './DataMaker/hdf5_train_data_list.txt')
+			line = line.replace('{TEST_PATH}', './DataMaker/hdf5_test_data_list.txt')
 			line = line.replace('{LABEL}', 'label')
 			line = line.replace('{DATA}', 'pose_data')
 			fout.write(line)
@@ -14,8 +14,8 @@ with open('res50_train_val_dy.prototxt') as fin:
 with open('res50_train_val_dy.prototxt') as fin:
         with open( 'resnet_orig.prototxt', 'wb') as fout:
                 for line in fin:
-                        line = line.replace('{TRAIN_PATH}', '../DataMaker/hdf5_train_data_list.txt')
-                        line = line.replace('{TEST_PATH}', '../DataMaker/hdf5_test_data_list.txt')
+                        line = line.replace('{TRAIN_PATH}', './DataMaker/hdf5_train_data_list.txt')
+                        line = line.replace('{TEST_PATH}', './DataMaker/hdf5_test_data_list.txt')
                         line = line.replace('{LABEL}', 'label')
                         line = line.replace('{DATA}', 'orig_data')
                         fout.write(line)
@@ -41,6 +41,6 @@ with open('res50_solver_dy.prototxt') as fin:
 os.mkdir('./trainlog')
 
 ### run training
-os.system('~/caffe/build/tools/caffe train --solver ./res50_solver_pose.prototxt --weights ./Models/ResNet-50-model.caffemodel --gpu=1 2>&1 | tee ./trainlogs/ResNet_pose16_' )
+os.system('~/library/caffe/build/tools/caffe train --solver ./res50_solver_pose.prototxt --weights ./Models/ResNet-50-model.caffemodel --gpu=1 2>&1 | tee ./trainlogs/ResNet_pose16_' )
 
-os.system('~/caffe/build/tools/caffe train --solver ./res50_solver_orig.prototxt --weights ./Models/ResNet-50-model.caffemodel --gpu=1 2>&1 | tee ./trainlogs/ResNet_orig16_' )
+os.system('~/library/caffe/build/tools/caffe train --solver ./res50_solver_orig.prototxt --weights ./Models/ResNet-50-model.caffemodel --gpu=1 2>&1 | tee ./trainlogs/ResNet_orig16_' )
